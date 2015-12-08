a9a49dbf93b7300f0a3ef1e133db5003712c61bd
tin-pot <mh@tin-pot.net>
2015-12-09 00:51:28 +0100

    VC9 Define USE_CBOR_CONTEXT=1 (Just to check it works).

--- include/cn-cbor/porting.h
+++ include/cn-cbor/porting.h
@@ -8,7 +8,8 @@
 # define true        1
 # define false       0
 
-typedef unsigned  char   uint8_t;
+typedef unsigned  char    uint8_t;
+
 typedef unsigned  short  uint16_t;
 typedef signed    short   int16_t;
 typedef unsigned  int    uint32_t;
